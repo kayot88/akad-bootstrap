7bbe4109e904dab92e0483e4434a2d4d3bc33731
kayot88 <kayot88@gmail.com>
2018-03-23 13:11:02 +0200

    first

---
+++ gulpfile.js
@@ -0,0 +1,213 @@
+'use strict';
+
+var gulp     = require('gulp'),
+sass 		 = require('gulp-sass'),
+prefixer = require('gulp-autoprefixer'),
+watch 		 = require('gulp-watch'),
+browserSync = require('browser-sync'),
+reload = browserSync.reload,
+include = require('gulp-include'),
+notify       = require('gulp-notify'),
+imagemin = require('gulp-imagemin'),
+pngquant = require('imagemin-pngquant'),
+//uglify 		 = require('gulp-uglify'),
+gcmq = require('gulp-group-css-media-queries'),
+babel       = require('gulp-babel'),
+smartgrid    = require('smart-grid'),  
+sourcemaps   = require('gulp-sourcemaps');
+
+var paths= {
+	src: {
+		html: ["*.html"],
+		css: "sass/style.scss",
+		img: "img/**/*.*",
+		fonts: "fonts/**/*.*",
+		js_main: "js/script.js",
+		js: "js/",		
+		images: "img/**/*.*"
+	},
+	dest: {
+		css: "css/",
+		html: "dist/",
+		images: "dist/img/",
+		js: "dist/js/"
+	},
+	watch: {
+		css: "sass/**/*.scss",
+		html: "*.html",
+		images: "img/**/*.*",
+		js: "js/script.js"
+	}
+	// ,
+	// bootstrap: './node_modules/bootstrap/dist/js/',
+	// jquery: './node_modules/jquery/dist/',
+	// isotop: './node_modules/isotope-layout/dist/',
+}
+
+gulp.task('css', function(){
+	gulp.src(paths.src.css)
+	.pipe(sass({
+		//outputStyle: 'compressed',
+		sourceMap: true,
+		errorToConsole: true }))
+	.pipe(prefixer())
+	.pipe(sourcemaps.init())
+	.pipe(sourcemaps.write())
+	.pipe(gulp.dest(paths.dest.css))
+	.pipe(reload({stream: true}))
+});
+
+gulp.task('html', function(){
+	gulp.src(paths.src.html)
+	.pipe(include({
+		extensions: "html",
+		hardFail: true,
+	})).on('error', notify.onError(
+	{
+		message: "<%= error.message %>",
+		title  : "HTML build error!"
+	}
+	)
+	)
+	.pipe(gulp.dest(paths.dest.html))
+	.pipe(reload({stream: true}))
+});
+
+gulp.task('images', function () {
+	gulp.src(paths.src.images)
+	.pipe(imagemin({
+		progressive: true,
+		svgoPlugins: [{removeViewBox: false}],
+		use: [pngquant()],
+		interlaced: true
+	}))
+	.pipe(gulp.dest(paths.dest.images))
+	.pipe(reload({stream: true}));
+});
+
+// gulp.task('js', function () {
+// 	gulp.src(paths.src.js_main)
+// 	.pipe(include({
+// 		extensions: "js",
+// 		hardFail: true,
+// 		includePaths: [paths.bootstrap, paths.isotop, paths.jquery, paths.src.js]
+// 	}).on('error', notify.onError(
+// 	{
+// 		message: "<%= error.message %>",
+// 		title  : "JS Error!"
+// 	}
+// 	)
+// 	)
+// 	)
+// 	.pipe(gulp.dest(paths.dest.js))
+// 	.pipe(reload({stream: true}));
+// 	});
+gulp.task('js', function () {
+    return gulp.src(paths.src.js_main)
+   // .pipe(uglify()) не нужен пока(минимизатор)
+   .pipe(gulp.dest(paths.dest.js));
+});
+
+gulp.task('js-watch', ['js'], function (done) {
+    browserSync.reload();
+    done();
+});
+
+gulp.task('watch', function() {
+	gulp.watch([paths.watch.css], function(event, cb){
+		gulp.start('css');
+	});
+
+	gulp.watch([paths.watch.html], function(event, cb){
+		gulp.start('html');
+	});
+
+  gulp.watch("js/*.js", ['js-watch']);
+	// gulp.watch("js/script.js", function(event, cb){
+	// 	gulp.start('babel');
+	// });
+	gulp.watch("js/script.js", ['babel']);
+
+	gulp.watch([paths.watch.images], function(event, cb){
+		gulp.start('images');
+	});
+
+	// gulp.watch([paths.watch.js], function(event, cb){
+	// 	gulp.start('js');
+	// });
+});
+
+gulp.task('refresh', function(){
+	browserSync({
+		server: {
+			baseDir: './'
+		},
+		injectChanges: true
+	});
+});
+
+gulp.task('build', [
+	'html', 
+	'css',
+	'images',
+	'js'
+	]);
+
+gulp.task('default', ['build', 'refresh', 'watch']);
+
+
+
+
+
+//	gulp.watch("./js/script.js", ['babel']);
+//babel
+gulp.task('babel', () =>
+	gulp.src('js/script.js')
+	.pipe(babel({
+		presets: ['env']
+	}))
+	.pipe(gulp.dest('dist'))
+	);
+
+//smart grid
+// var smartGridSettings = {
+// 	outputStyle: 'scss',
+// 	filename: '_smart-grid', 
+// 	columns: 24, 
+// 	offset: "12px", 
+// 	container: {
+// 		maxWidth: '1092px',
+// 		fields: '12px' 
+// 	},
+// 	breakPoints: {
+// 		lg: {
+// 			'width': '1000px', 
+// 			'fields': '12px' 
+// 		},
+// 		md: {
+// 			'width': '960px',
+// 			'fields': '15px'
+// 		},
+// 		sm: {
+// 			'width': '780px',
+// 			'fields': '15px'
+// 		},
+// 		xs: {
+// 			'width': '660px',
+// 			'fields': '15px'
+// 		}
+// 	}
+// };
+
+// gulp.task('smartgrid', function() {
+// 	smartgrid('sass/base', smartGridSettings);
+// });
+
+//gulp.task('watch', ['smartgrid']);
+
+//gcmq
+// gulp.task('gcmq', function () {
+//     gulp.src('app/css/main.min.css')
+//         .pipe(gcmq())
+//         .pipe(gulp.dest('dist/css'));
+// })
---
+++ js/script.js
@@ -0,0 +1,128 @@
+//=require isotope.pkgd.min.js
+$(document).ready(function() {
+
+  var container = document.getElementById('container');
+  var inner = document.getElementById('inner');
+
+  var onMouseEnterHandler = function(event) {
+    update(event);
+  };
+  var onMouseLeaveHandler = function() {
+    inner.style = "";
+  };
+  var onMouseMoveHandler = function(event) {
+    if (isTimeToUpdate()) {
+      update(event);
+    }
+  };
+
+  container.onmouseenter = onMouseEnterHandler;
+  container.onmouseleave = onMouseLeaveHandler;
+  container.onmousemove = onMouseMoveHandler;
+
+  var counter = 0;
+  var updateRate = 10;
+  var isTimeToUpdate = function() {
+    return counter++ % updateRate === 0;
+  };
+
+// Init
+var container = document.getElementById('container');
+var inner = document.getElementById('inner');
+// Mouse 
+var mouse = {
+  _x: 0,
+  _y: 0,
+  x: 0,
+  y: 0,
+  updatePosition: function(event) {
+    var e = event || window.event;
+    this.x = e.clientX - this._x;
+    this.y = (e.clientY - this._y) * -1;
+  },
+  setOrigin: function(e) {
+    this._x = e.offsetLeft + Math.floor(e.offsetWidth/2);
+    this._y = e.offsetTop + Math.floor(e.offsetHeight/2);
+  },
+  show: function() { return '(' + this.x + ', ' + this.y + ')'; }
+}
+// Track the mouse position relative to the center of the container.
+mouse.setOrigin(container);
+
+var update = function(event) {
+  mouse.updatePosition(event);
+  updateTransformStyle(
+    (mouse.y / inner.offsetHeight/2).toFixed(2),
+    (mouse.x / inner.offsetWidth/2).toFixed(2)
+    );
+};
+
+var updateTransformStyle = function(x, y) {
+  var style = "rotateX(" + x + "deg) rotateY(" + y + "deg)";
+  inner.style.transform = style;
+  inner.style.webkitTransform = style;
+  inner.style.mozTransform = style;
+  inner.style.msTransform = style;
+  inner.style.oTransform = style;
+};
+$('.nav-toggle').on('click', function() {
+  $('.nav').toggleClass('open');
+  $('.nav>.interior>.nav-toggle').toggleClass('nav-toggle--open');
+  $('#container').toggleClass('open'); 
+  // var elem = document.getElementById('container');
+  // elem.style.cssText += 'z-index: -1;';
+});
+
+const crazyButtons = document.querySelectorAll('.btn-crazy');
+
+    // define our functions
+    function goCrazy() {
+      // get a random number for the left offset
+      // random num for top offset
+      const offsetLeft = Math.random() * (window.innerWidth - this.clientWidth);
+      const offsetTop  = Math.random() * (window.innerHeight - this.clientHeight);
+
+      console.log(offsetLeft, offsetTop);
+
+      // apply those numbers to the button
+      this.style.top = offsetTop + 'px';
+      this.style.left = offsetLeft + 'px';
+    }
+
+    // add event listeners
+    crazyButtons.forEach(button => button.addEventListener('mouseenter', goCrazy));
+
+//fullscroll
+
+
+});
+
+var $isotopeGrid;
+$(document).ready(function() {
+  $('.choose-item > a').on('click', function(ev) { 
+    ev.preventDefault();
+
+    var filterValue = $(this).data('filter');
+    $isotopeGrid.isotope({ filter: filterValue });
+    $(this).addClass('active').parent('li').siblings().find('a').removeClass('active');
+
+  }); 
+})
+
+  $(window).on('load', function(){  
+    $isotopeGrid =  $('.portfolio__grid').isotope({  
+      itemSelector: '.portfolio__item',   
+      percentPosition: true,   
+      masonry: {     
+        columnWidth: '.portfolio__sizer'    
+      } 
+    })
+  });
+
+
+
+ //  $('.st-block1__link').on('click', function(e) { 
+ //    e.preventDefault();
+ //    $(this).css(!{background-color: transparent});
+ //  });
+ // 
